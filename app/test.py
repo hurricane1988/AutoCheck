45e194611653c20d0797db53062284fd84d33cc9
Hurricane <16606960+hurricane1988@users.noreply.github.com>
2020-03-15 20:16:45 +0800

    add files

--- app/test.py
+++ app/test.py
@@ -36,10 +36,20 @@ c.close()
 
 import sqlite3
 from Configuration import *
-from pyecharts import Bar
+from pyecharts import options as opts
+from pyecharts.charts import Bar
+from pyecharts.faker import Faker
 
 
-bar = Bar('地图','这是副标题')
-bar.add("服装",["衬衫", "羊毛衫", "雪纺衫", "裤子", "高跟鞋", "袜子"], [5, 20, 36, 10, 75, 90])
-bar.render()
+c = (
+    Bar()
+    .add_xaxis(Faker.choose())
+    .add_yaxis("商家A", Faker.values())
+    .add_yaxis("商家B", Faker.values())
+    .set_global_opts(
+        title_opts=opts.TitleOpts(title="Bar-Brush示例", subtitle="我是副标题"),
+        brush_opts=opts.BrushOpts(),
+    )
+    .render("bar_with_brush.html")
+)
 
