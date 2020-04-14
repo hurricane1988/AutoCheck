ea9d1c4e6f906aed19a8282fd84007af6e2b0cd1
Hurricane <16606960+hurricane1988@users.noreply.github.com>
2020-04-14 15:53:26 +0800

    update python files

--- app/CallenterInfo.py
+++ app/CallenterInfo.py
@@ -7,31 +7,37 @@
 # FileName: CallenterInfo.py
 ########################################
 
-import sys,socket,sqlite3
-from Configuration import *
+import socket
+from Configuration import connect,cursor
 from PyQt5 import QtWidgets, QtCore, QtGui
-from enterInfo import Ui_enterInfo
+from enterInfo import Ui_EnterInfo
 
 
-class enterWindow(QtWidgets.QWidget,Ui_enterInfo):
+# 信息录入主类.
+class enterWindow(QtWidgets.QWidget,Ui_EnterInfo):
     def __init__(self, parent=None):
         super(enterWindow, self).__init__(parent)
+        self.setWindowIcon(QtGui.QIcon('login.ico'))
+        self.setFixedSize(489,359)
 
+        # 加载图形化主函数.
         self.setupUi(self)
-
-        self.pushButtonEntry.clicked.connect(self.ensureEntry)
-        self.pushButtonQuit.clicked.connect(self.close)
+        # 服务器信息录入.
+        self.pushButtonServerEntry.clicked.connect(self.ServersEntry)
+        self.pushButtonServerQuit.clicked.connect(self.close)
+        # 数据库信息录入.
+        self.pushButtonDBEntry.clicked.connect(self.DatabaseEntry)
+        self.pushButtonDBQuit.clicked.connect(self.close)
 
     # 信息录入主函数.
-    def ensureEntry(self):
-        self.ostype = self.comboBoxSelectOS.currentText()
-        self.version = self.comboBoxSelectVersion.currentText()
-        self.IP = self.lineEditIP.text()
-        self.username = self.lineEditUserName.text()
-        self.password = self.lineEditPassWord.text()
-        self.password_ensure = self.lineEditEnsure.text()
-        self.description = self.plainTextEditDescription.toPlainText()  # 获取plainTextEdit值方法
-        #self.plainTextEditDescription.setPlainText()  # 设置plainTextEdit值方法
+    def ServersEntry(self):
+        self.ostype = self.comboBoxSelectServerOS.currentText()
+        self.version = self.comboBoxSelectServerVersion.currentText()
+        self.IP = self.lineEditServerIP.text()
+        self.username = self.lineEditServerUserName.text()
+        self.password = self.lineEditServerPassWord.text()
+        self.password_ensure = self.lineEditServerEnsure.text()
+        self.description = self.plainTextEditServerDescription.toPlainText()  # 获取plainTextEdit值方法
 
         # 判断输入的IP格式是否正确.
         if self.password == self.password_ensure:
@@ -46,6 +52,7 @@ class enterWindow(QtWidgets.QWidget,Ui_enterInfo):
                     pass
                 finally:
                     pass
+
             except AttributeError:
                 try:
                     socket.inet_aton(self.IP)
@@ -54,16 +61,34 @@ class enterWindow(QtWidgets.QWidget,Ui_enterInfo):
             except socket.error:
                 QtWidgets.QMessageBox.warning(self,'错误提示','输入的IP地址为非法格式',QtWidgets.QMessageBox.Cancel)
 
-        elif self.password is not self.password_ensure:
-            QtWidgets.QMessageBox.warning(self,'错误提示','两次输入的密码不一致',QtWidgets.QMessageBox.Cancel)
+    # 数据库信息录入主函数.
+    def DatabaseEntry(self):
+        self.dbtype = self.comboBoxSelectDB.currentText()
+        self.dbversion = self.comboBoxSelectDBVersion.currentText()
+        self.dbip = self.lineEditDBIP.text()
+        self.dbusername = self.lineEditDBUserName.text()
+        self.dbname = self.lineEditDatabaseName.text()
+        self.dbpassword = self.lineEditDBPassWord.text()
+        self.dbpassword_ensure = self.lineEditDBEnsure.text()
+        self.dbdescription = self.plainTextEditDBDescription.toPlainText()
 
-        elif self.version == '':
-            QtWidgets.QMessageBox.warning(self, '错误提示', '系统版本未填写', QtWidgets.QMessageBox.Cancel)
-        elif self.IP == '':
-            QtWidgets.QMessageBox.warning(self, '错误提示', 'IP地址未填写', QtWidgets.QMessageBox.Cancel)
-        elif self.username == '':
-            QtWidgets.QMessageBox.warning(self, '错误提示', '账号未填写', QtWidgets.QMessageBox.Cancel)
-        elif self.password == '' or self.password_ensure == '':
-            QtWidgets.QMessageBox.warning(self, '错误提示', '密码为填写', QtWidgets.QMessageBox.Cancel)
-        elif self.description == '':
-            QtWidgets.QMessageBox.warning(self, '错误提示', '服务器用途未填写', QtWidgets.QMessageBox.Cancel)
+        if self.dbpassword == self.dbpassword_ensure:
+            try:
+                socket.inet_pton(socket.AF_INET,self.dbip)
+                try:
+                    cursor.execute("insert into t_database (ip,dbtype,dbversion,dbname,username,password,desc) values ('{}','{}','{}','{}','{}','{}','{}')".format(self.dbip,self.dbtype,self.dbversion,self.dbname,self.dbusername,self.dbpassword,self.dbdescription))
+                    connect.commit()
+                    QtWidgets.QMessageBox.information(self, '消息提示', '成功录入信息', QtWidgets.QMessageBox.Cancel)
+                except Exception as e:
+                    QtWidgets.QMessageBox.warning(self,'错误提示',"信息录入失败\n'{0}".format(e),QtWidgets.QMessageBox.Cancel)
+                    pass
+                finally:
+                    pass
+
+            except AttributeError:
+                try:
+                    socket.inet_aton(self.dbip)
+                except socket.error:
+                    return False
+            except socket.error:
+                QtWidgets.QMessageBox.warning(self,'错误提示','输入的IP地址为非法格式',QtWidgets.QMessageBox.Cancel)
